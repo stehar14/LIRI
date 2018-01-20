1b90c50161a8f32c656589671c87f31c7de513ce
Steve Harold <stehar14@yahoo.com>
2018-01-20 03:17:30 -0500

    Added more random commands to random.txt and split them in the do-what-it-says function.

--- liri.js
+++ liri.js
@@ -1,64 +1,65 @@
+// Loading various npms, file system, keys.js
 require("dotenv").config();
 var fs = require('fs');
 var request = require('request');
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
+// Creating Twitter and Spotify clients using keys
 var client = new Twitter(keys.twitter);
 var spotify = new Spotify(keys.spotify);
-var userService;
 var search;
 //console.log(spotify);
 //console.log(client);
-// First I want to read the file
 
 var userService = process.argv[2];
 
+// Main switch case evaluating the contents of process.argv[2]
 switch(userService) {
 	case "my-tweets":
-		//twitter stuff
 		tweeter();
 		break;
 	case "spotify-this-song":
-		//spotify stuff
 		spotter();
 		break;
 	case "movie-this":
-		//omdb stuff
 		movier();
 		break;
 	case "do-what-it-says":
-		//random.txt stuff
 		randomer();
 		break;
 	default:
-		console.log("Sorry, I didn't recognize that command. Please choose one of my four functions: 'my-tweets', 'spotify-this-song', 'movie-this', or 'do-what-it-says'.");
+		console.log('Sorry, I do not recognize that command. Please choose one of my four functions: my-tweets; spotify-this-song "song to search", movie-this "movie to search", or do-what-it-says.');
 }
 
-//twitter stuff
+// Function to run when process.argv[2] is "my-tweets"
 function tweeter() {
-	console.log("twitter");
+	//console.log("twitter");
 	var params = {screen_name: 'S_Harold_Dev'};
+	// Getting tweets from S_Harold_Dev account
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	// IF there aren't any errors, display latest tweets (max 20)		
 			if (!error) {
-			for (i=0; i<20 && i<tweets.length; i++){
-				console.log("                                                                 ");
-				console.log("+++++Tweet #" + (i+1) + "+++++");
-				console.log("Tweeted at: " + tweets[i].created_at);
-				console.log("                                                                ");
-				console.log(tweets[i].text);
-				console.log("                                                                ");
-				console.log("*******************************************************************");
+				for (i=0; i<20 && i<tweets.length; i++){
+					console.log("                                                                 ");
+					console.log("+++++Tweet #" + (i+1) + "+++++");
+					console.log("Tweeted at: " + tweets[i].created_at);
+					console.log("                                                                ");
+					console.log(tweets[i].text);
+					console.log("                                                                ");
+					console.log("*******************************************************************");
+					log("Tweeted at: " + tweets[i].created_at + "   Tweet: " + tweets[i].text);
 				}
 			}
 	});
 }
 
+// Function to run when process.argv[2] is
 function spotter() {
 	console.log("spotify");
 	if (process.argv[3] === undefined && search === undefined){
 		
-		trackName = "the sign";
+		trackName = "ace the sign";
 	} else if (search !== undefined) {
 		trackName = search;
 	} else {
@@ -70,46 +71,62 @@ function spotter() {
 			return console.log('Error occurred: ' + err);
 			}
 
-		console.log(data.tracks.items[0].artists[0].name); 
-		console.log(data.tracks.items[0].name);
-		console.log(data.tracks.items[0].href);
-		console.log(data.tracks.items[0].album.name);
+		console.log("Artist: " + data.tracks.items[0].artists[0].name);
+		log("Artist: " + data.tracks.items[0].artists[0].name); 
+		console.log("                                                                ");
+		console.log("Song: " + data.tracks.items[0].name);
+		log("Song: " + data.tracks.items[0].name);
+		console.log("                                                                ");
+		console.log("Album: " + data.tracks.items[0].album.name);
+		log("Album: " + data.tracks.items[0].album.name);
+		console.log("                                                                ");
+		console.log("Link to song: " + data.tracks.items[0].href);
+		log("Link to song: " + data.tracks.items[0].href);
+		log("                                                                        ");
 	});
 }
 
 function movier() {
 	if (process.argv[3] === undefined && search === undefined){
 		title = "Mr. Nobody";
-	} else if (process.argv[3] !== undefined) {
-		title = process.argv[3];
-	} else {
+	} else if (search !== undefined) {
 		title = search;
+	} else {
+		title = process.argv[3];
 	}
 	request("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
 		if (!error && response.statusCode === 200) {
 
 			if (JSON.parse(body).Title) {
+				log("Title: " + JSON.parse(body).Title);
 				console.log("Title: " + JSON.parse(body).Title);
 			}
-			if (JSON.parse(body).Released) {	
+			if (JSON.parse(body).Released) {
+				log("Release date: " + JSON.parse(body).Released);	
 				console.log("Release date: " + JSON.parse(body).Released);
 			}
-			if (JSON.parse(body).Ratings[0]) {	
+			if (JSON.parse(body).Ratings[0]) {
+				log("IMDb Rating: " + JSON.parse(body).Ratings[0].Value);	
 				console.log("IMDb Rating: " + JSON.parse(body).Ratings[0].Value);
 			}
 			if (JSON.parse(body).Ratings[1]) {
+				log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
 				console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
 			}
 			if (JSON.parse(body).Country) {
+				log("Country produced: " + JSON.parse(body).Country);
 				console.log("Country produced: " + JSON.parse(body).Country);
 			}
 			if (JSON.parse(body).Language) {
+				log("Language: " + JSON.parse(body).Language);
 				console.log("Language: " + JSON.parse(body).Language);
 			}
 			if (JSON.parse(body).Plot) {
+				log("Plot: " + JSON.parse(body).Plot);
 				console.log("Plot: " + JSON.parse(body).Plot);
 			}
 			if (JSON.parse(body).Actors) {
+				log("Actors: " + JSON.parse(body).Actors);
 				console.log("Actors: " + JSON.parse(body).Actors);
 			}
 		}
@@ -117,34 +134,38 @@ function movier() {
 }
 
 function randomer() {
-	console.log("random");
+	//console.log("random");
 	fs.readFile('random.txt', 'utf-8', function read(err, data) {
-		if (err) {
-			return console.log(err);
-		}
-		var randCommand = data.split(",");
-		userService = randCommand[0];
-		console.log(userService);
-		search = randCommand[1];
+		var randSet = data.split(";");
+		randomNum = Math.floor(Math.random() * randSet.length);
+			var randCommand = randSet[randomNum].split(",");
+			userService = randCommand[0];
+			console.log(userService);
+			search = randCommand[1];
+			console.log(userService);
+			switch(userService) {
+				case "my-tweets":
+					//twitter stuff
+					tweeter();
+					break;
+				case "spotify-this-song":
+					//spotify stuff
+					//console.log(search);
+					spotter();
+					break;
+				case "movie-this":
+					//omdb stuff
+					movier();
+					break;
+				default:
+					console.log('Sorry, I do not recognize that command. Please choose one of my four functions: my-tweets; spotify-this-song "song to search", movie-this "movie to search", or do-what-it-says.');
+			}
+		
+	});
+}
 
-	
-	console.log(userService);
-	switch(userService) {
-		case "my-tweets":
-			//twitter stuff
-			tweeter();
-			break;
-		case "spotify-this-song":
-			//spotify stuff
-			console.log(search);
-			spotter();
-			break;
-		case "movie-this":
-			//omdb stuff
-			movier();
-			break;
-		default:
-			console.log("Sorry, I didn't recognize that command. Please choose one of my four functions: 'my-tweets', 'spotify-this-song', 'movie-this', or 'do-what-it-says'.");
-	}
-});
+function log(toAppend) {
+  	fs.appendFile('log.txt', toAppend , function (err) {
+  		if (err) throw err;
+	});
 }
